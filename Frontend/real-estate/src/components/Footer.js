df7e4f1e0822f47f3e28dc5604492de7b9d3ac22
Fuad <issafuad1818@gmail.com>
2024-02-09 07:49:26 -0800

    hosting

--- Frontend/real-estate/src/components/Footer.js
+++ Frontend/real-estate/src/components/Footer.js
@@ -38,13 +38,10 @@ const Footer = () => {
         </div>
         <div className="footer__col">
           <h4>Company</h4>
-          <Link to="https://www.compass.com/agents/litvak-team/">
-            <li>Compass</li>
+          <Link to="https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2F2349036494088&e=AT1A2RQLmgOb4RCZA5713BTqLYUSooVk6RhCtYNmPCtLxrsZnu6iDqZeCYNOcNg8y1BGneaCiRZwpG-G-p9K3yKBMvsu71p0">
+            <li>Whatsapp</li>
           </Link>
-          <Link to="https://www.facebook.com/thelitvakteam/">
-            <li>Facebook</li>
-          </Link>
-          <Link to="https://www.instagram.com/thelitvakteam/">
+          <Link to="https://www.instagram.com/bimbolaproperties/">
             <li>Instagram</li>
           </Link>
         </div>
